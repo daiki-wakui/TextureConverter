ce7f4e6cba162a99b9d81482785e804b8d17bac7
daiki-wakui <k021g1119@g.neec.ac.jp>
2023-10-02 15:15:02 +0900

    02_03確認まで

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+int main(int argc, char* argv[]) {
 
 	printf("Hello,World!\n");
+
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		//•¶š—ñargv‚Ìi”Ô–Ú‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 	system("pause");
 
